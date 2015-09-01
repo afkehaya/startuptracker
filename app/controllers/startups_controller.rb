207bfd2be75bef306f9597b16b9518ea62b2f4a3
Alexander Kehaya <afkehaya@gmail.com>
2015-09-01 10:43:10 -0700

    added links to form and deleted restricted buttons

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,5 +1,5 @@
 class StartupsController < ApplicationController
- befor_action :authenticate_user!
+ before_action :authenticate_user!
 
 	def create
 		@startup = @startupable.startups.new startup_params
