6fa3c292afaf5c5e49e9182c84d541fec8d44d8f
Alexander Kehaya <afkehaya@gmail.com>
2015-11-20 19:32:27 -0800

    chagned route for devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
   
   resources :industries , only: [:show, :create, :edit, :update] do
     resources :categories do
